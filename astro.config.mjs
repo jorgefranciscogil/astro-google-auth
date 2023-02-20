4c2d78c8ee5e65ce4a67c04e0627cf5689867f61
Jorge Francisco Gil <jorgefrancisco.gil.contractor@bbva.com>
2023-02-20 04:41:39 +0100

    feat(*): remove netlify adapter; add vercel adapter;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: vercel(),
 });
